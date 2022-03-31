143780d8d833e252380229d46e2cb24a28106df6
anna-jonsson <anna.f.jonsson@gmail.com>
2022-03-31 16:33:43 +0200

    Added handling for dark/light mode
    -Added logos for dark and light mode to be switched when toggling
    -Added handling for localStorage to save and fetch dark and light mode changes

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 //Link to Github repository: https://github.com/anna-jonsson/U3_Jonsson_Anna
 
+//Global variable for button and event listener to handle dark mode / light mode
+
+let themeBtn = document.querySelector('button')
+let body = document.querySelector('body')
+
+themeBtn.addEventListener('click', function () {
+  body.classList.toggle('theme')
+  themeBtn.classList.toggle('themeBtn')
+  setTheme()
+})
+
+function setTheme () {
+  if (body.className.includes('theme')) {
+    localStorage.setItem('dark-mode', 'true')
+  } else {
+    localStorage.setItem('dark-mode', 'false')
+  }
+}
+
+window.onload = function () {
+  document.querySelector('input').value = ''
+
+  let theme = localStorage.getItem('dark-mode')
+
+  if (theme == 'true') {
+    body.classList.add('theme')
+    themeBtn.classList.add('themeBtn')
+  }
+}
